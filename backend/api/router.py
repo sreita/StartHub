e5c3cdb81f2866420fbf40cc3e7008f3169a2c3e
sreita <sreita@unal.edu.co>
2025-11-20 18:34:01 -0500

    Add search implementation

--- backend/api/router.py
+++ backend/api/router.py
@@ -1,7 +1,8 @@
 from fastapi import APIRouter
-from backend.api.routes import comments, votes, dev
+from backend.api.routes import comments, votes, dev, search
 
 api_router = APIRouter()
 api_router.include_router(comments.router, prefix="/comments", tags=["comments"])
 api_router.include_router(votes.router, prefix="/votes", tags=["votes"])
 api_router.include_router(dev.router, prefix="/dev")
+api_router.include_router(search.router, prefix="/startups", tags=["search"])
