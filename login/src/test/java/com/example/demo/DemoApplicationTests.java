24e00ea196eb12a054c03943f96195c9c2047b2f
dcamelos <dcamelos@unal.edu.co>
2025-11-25 20:45:56 -0500

    fix demo test

--- login/src/test/java/com/example/demo/DemoApplicationTests.java
+++ login/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -2,7 +2,11 @@ package com.example.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+
+
+@ActiveProfiles("test")
 @SpringBootTest
 class DemoApplicationTests {
 
